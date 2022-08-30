6e383ad6c987ef9b0daa52d36db56c2ef1468344
nakta <nakta0511@gmail.com>
2022-08-30 13:14:45 +0900

    first commit

---
+++ src/main/java/hello/itemservice/domain/item/Item.java
@@ -0,0 +1,21 @@
+package hello.itemservice.domain.item;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter @Setter
+@NoArgsConstructor
+public class Item {
+
+    private Long id;
+    private String name;
+    private Integer price;
+    private Integer quantity;
+
+    public Item(String name, Integer price, Integer quantity) {
+        this.name = name;
+        this.price = price;
+        this.quantity = quantity;
+    }
+}
